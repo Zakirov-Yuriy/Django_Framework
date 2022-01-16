7af97a31b52409ce42978c345108c1f94d4ec7a0
Юрий <70097423+Zakirov-Yuriy@users.noreply.github.com>
2022-01-16 17:43:08 +0300

    Task#3

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -129,6 +129,10 @@ STATICFILES_FINDERS = [
 
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from mainapp.models import Product
 
 def index(request):
     title = 'geekshop'
+    products = Product.objects.all()[:4]
 
     context = {
         'title': title,
+        'products': products,
     }
     return render(request, 'index.html', context=context)
 
